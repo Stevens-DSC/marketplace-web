e7158ad9feb1ae80d9961cbade2a3088216feaed
Justin O'Boyle <justin@justinoboyle.com>
2021-03-31 13:39:10 -0400

    add about

--- src/client/pages/about.js
+++ src/client/pages/about.js
@@ -7,34 +7,15 @@ const About = (props) => {
         <>
             <Head>
                 <title>About | Square Mile Market</title>
-                {/* <meta name="description" key="description" value={description} /> */}
+                <meta name="description" key="description" value={`Get to know us and our mission a little better`} />
             </Head>
             <h1>About us</h1>
-            <p>Lorem ipsum</p>
+            <p>Get to know us and our mission a little better!</p>
+            <h2>Who we are</h2>
+            <p>We are the Google Developer Student Club at Stevens Institute of Technology in Hoboken, New Jersey. We are a collection of students from all different grades and majors, who have come together to use what we know to create a response to the Google Solution Challenge.</p>
+            <h2>Our mission</h2>
+            <p>We are focused on addressing the United Nations Sustainable Development Goal #8 which outlines “[promoted] sustainability, inclusive, and sustainable economic growth, full and productive employment, and work for all”. We hope that our site will encourage people to shop local and put money back into their community’s economy. We aim to make looking local convenient and easy and hope to promote small businesses through our website.</p>
         </>
     )
 }
-
-// About.getInitialProps = async function (context) {
-//     // const name = context.query.id
-//     // try {
-//     //     const req = await api.getStoreByShortName({ shortname: name })
-//     //     return {
-//     //         loaded: true,
-//     //         success: true,
-//     //         error: false,
-//     //         store: req
-//     //     }
-//     // }catch(e) {
-//     //     return {
-//     //         loaded: true,
-//     //         success: false,
-//     //         error: e + ""
-//     //     }
-//     // }
-//     return {
-        
-//     }
-// }
-
 export default About
